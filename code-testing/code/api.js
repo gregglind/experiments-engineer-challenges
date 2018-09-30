3f788c7ecd9dbdbdb129a3fd173fe9f92948fbc0
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2018-09-30 15:52:22 -0400

    rename foo -> sumOfSquares & adding tests * added "engines" to package.json:   * assert custom error type requires >9   * process.hrtime.bigint requires >10.7   * lib can RUN on lower node version, tests will break on <10.7 * temp. disabled already failing openUrlsInOrder tests * moved module.exports to top for readability

--- code-testing/code/api.js
+++ code-testing/code/api.js
@@ -1,12 +1,57 @@
-
-
-// Part 1:  foo
-function foo(a) {
-  a = a || [];
-  var b = a.map(function(el) { return el * el });
-  return b.reduce(function(s, el) { return s + el }, 0);
+const assert = require('assert');
+module.exports = {
+  openUrlsInOrder,
+  sumOfSquares
 }
 
+/**
+ * Square a number
+ * Throws if square will be greater than Number.MAX_VALUE (will not return infinity)
+ * @param {Number} base number to be squared
+ * @return {Number} square of base
+ */
+function square(base){
+  assert(typeof base === 'number', new TypeError('base must be a Number'));
+  assert(!Number.isNaN(base), 'cannot square NaN');
+  const power = Math.pow(base, 2);
+  assert(Number.isFinite(power), new RangeError('base too large to square'))
+  return power;
+}
+
+/**
+ * Add two numbers together.
+ * Strict about inputs being numbers & not NaN
+ * Throws if sum is greater than Number.MAX_VALUE (will not return infinity)
+ * @param  {Number} augend
+ * @param  {Number} addend
+ * @return {Number} sum of the two
+ */
+function add(augend, addend){
+  assert(typeof augend === 'number', new TypeError('augend must be a Number'));
+  assert(!Number.isNaN(augend), 'augend cannot be NaN');
+  assert(typeof addend === 'number', new TypeError('addend must be a Number'));
+  assert(!Number.isNaN(addend), 'addend cannot be NaN');
+  let sum = augend + addend;
+  assert(Number.isFinite(sum), new RangeError('input numbers too large: sum greeter than infinity'))
+  return sum;
+}
+
+/**
+ * Square over array of numbers & return the sum
+ * Returns 0 if empty array passed
+ * Throws if sum is greater than Number.MAX_VALUE (will not return infinity)
+ * Throws if non-array passed or if any elements are not finite numbers
+ * See `square` and `add` for more error conditions
+ * @param {Number[]} bases numbers to be squared & summed
+ * @return {Number} sum of the square of the bases, or 0 if empty array passed
+ */
+function sumOfSquares(bases) {
+  assert(Array.isArray(bases), new TypeError('bases must be an array'));
+  if(!bases.length){ return 0; }
+  return bases
+    .map(square)
+    .reduce(add, 0);
+}
 
 // Part 2:  openTabsInOrder
 
@@ -31,10 +76,3 @@ async function openUrlsInOrder (urlsArray) {
   work.push(['done',true]);
   return work;
 }
-
-module.exports = {
-  openUrlsInOrder,
-  foo
-}
-
-
--- code-testing/test/api.js
+++ code-testing/test/api.js
@@ -1,24 +1,84 @@
-// assert is like assert, but with some extra functions.
 const assert = require("assert");
 const {
-  foo,
+  sumOfSquares,
   openUrlsInOrder
 } = require('../code/api.js');
 
+/**
+ * creates array of descending numbers, from limit to last number >=1
+ * decremented by 1 until number is less than 1
+ * (will optimize for tail-call once Node adds that feature)
+ * @param {Number} limit largest number to use
+ * @param {Number[]} acc
+ */
+function range(limit, acc=[]){
+  if(limit < 1) { return acc; }
+  return range(limit - 1, acc.concat(limit));
+}
 
-// part 1: foo
-describe('foo()', function () {
-  it("should do something", function () {
-    assert(false, "foo has no real tests");
+// part 1: sumOfSquares
+describe('sumOfSquares()', function () {
+  it('returns 0 if array is empty',() => {
+    assert.strictEqual(sumOfSquares([]), 0);
   })
-  it("should do something else", function () {
-    assert(false, "foo has no real tests");
+  it('works with an array of length 1',() => {
+    assert.strictEqual(sumOfSquares([2]), 4);
   })
+  it('works with integers',() => {
+    assert.strictEqual(sumOfSquares([2, 4, 6, 8]), 120);
+  })
+  it('works with floats',() => {
+    assert.strictEqual(sumOfSquares([2.5, 0.9, .01]), 7.0601);
+  })
+  it('works with a mix of floats and integers',() => {
+    assert.strictEqual(sumOfSquares([2, 4, 6, 8, 2.5, 0.9, .01]), 127.0601);
+  })
+  it('works with negative numbers',() => {
+    assert.strictEqual(sumOfSquares([-2, 4, -6, 8, -2.5, 0.9, -.01]), 127.0601);
+  })
+  it('throws if any items in the array are not numbers',() => {
+    assert.throws(() => sumOfSquares([1,2,3,"four"]), TypeError);
+    assert.throws(() => sumOfSquares([null,1,2,3]), TypeError);
+    assert.throws(() => sumOfSquares([{a:1},2,3]), TypeError);
+  })
+  it('throws if any items in the array are NaN',() => {
+    assert.throws(() => sumOfSquares([1,2,3,NaN]), assert.AssertionError);
+    assert.throws(() => sumOfSquares([Math.pow(1,"abc")]), assert.AssertionError);
+  })
+  it('throws if no argument is passed',() => {
+    assert.throws(() => sumOfSquares(), TypeError);
+  })
+  it('throws if a non-array value is passed',() => {
+    assert.throws(() => sumOfSquares(1,2,3), TypeError);
+    const arrayLike = {0: 1, 1: 12, length: 2}
+    assert.throws(() => sumOfSquares(arrayLike), TypeError);
+  })
+  it('throws if nulls are in the array',() => {
+    assert.throws(() => sumOfSquares([1,2,3, null]), TypeError);
+  })
+  it('throws if a too-large-to-square number is passed', () =>{
+    const tooLargeToSquare = Number.MAX_VALUE;
+    assert.throws(() => sumOfSquares([1,2,tooLargeToSquare]), RangeError);
+  });
+  it('throws if bases produce powers too large to sum', () =>{
+    assert.doesNotThrow(() => sumOfSquares([1e+154]), 'not throwing on square');
+    assert.throws(() => sumOfSquares([1e+154,1e+154]), RangeError, 'throwing on summing the powers');
+  });
+  // added to check for performance regressions
+  // @TODO verify that this works on CI platform, other devs computers etc.
+  it('processes 2000 numbers in <100ms', () =>{
+    const input = range(2000);
+    const start = process.hrtime.bigint();
+    sumOfSquares(input);
+    const end = process.hrtime.bigint();
+    const millis = Number(end - start) / 1000000;
+    assert.ok(millis < 100);
+  });
 });
 
 
 // part 2:  openUrlsInOrder
-describe('openUrlsInOrder()', function () {
+xdescribe('openUrlsInOrder()', function () {
   it('should open urls in order', async function () {
 
     const urls = ['url1', 'url2', 'url3', 'url4']
